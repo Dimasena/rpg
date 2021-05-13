aade89bf896406fd281730b2f7121827418d501a
Dilman <dilman.nurdila@gmail.com>
2021-05-13 16:19:37 +0600

    create window

--- main.py
+++ main.py
@@ -2,3 +2,22 @@ import pygame as pg
 
 
 pg.init()
+SCREEN_WIDTH = 1500
+SCREEN_HEIGHT = 1000
+screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+bg_color = 'white'
+
+running = True
+while running:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+
+
+    screen.fill(bg_color)
+
+
+
+
+    pg.display.flip()
